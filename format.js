85af845136e8538cb96109b1b482a1641f731a00
Alex <58665866+cainy19com@users.noreply.github.com>
2024-06-14 14:52:36 +0800

    Update format.js

--- format.js
+++ format.js
@@ -2,8 +2,14 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 window.onload = () => {
-  $$("code.language-math").forEach(code => {
-    const exp = code.innerText.replace("displaylines", "displaystyle")
+  $$("code.language-meth").forEach(code => {
+    const exp = code.innerText
     katex.render(exp, code, {throwOnError: false})
+  })
+  
+  $$("p").forEach(p => {
+    const text = p.innerText.trim()
+    if (text.startsWith("http"))
+      p.innerHTML = `<a href="${text}">${text}</a>`
   })
 }
