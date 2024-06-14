123d2f38680df11273f988de8ef64f6cc3374c86
Alex Potter <58665866+cainy19com@users.noreply.github.com>
2024-06-14 17:46:16 +0800

    Update format.js

--- katex/format.js
+++ katex/format.js
@@ -12,7 +12,11 @@ window.onload = () => {
     if (text.startsWith("http")) {
       p.innerHTML = `<a href="${text}">${text}</a>`
     }
-    else {
+    if (text.startsWith("https://cainy19com.github.io/")) {
+      const a = p.querySelector("a")
+      a.innerText = a.innerText.split("/").pop()
+    }
+    if (!text.startsWith("http")) {
       p.innerHTML = text.replaceAll(/\$([^\$]*)\$/gi, (m, m1) => {
         const span = document.createElement("span")
         katex.render(m1, span, {throwOnError: false})
