2c56dce5b238a86bc78689a7e0e3ee6721b16488
Alex <58665866+cainy19com@users.noreply.github.com>
2024-06-14 13:31:02 +0800

    Update format.js

--- format.js
+++ format.js
@@ -1,1 +1,8 @@
-alert(123)
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+document.addEventListener("DOMContentLoaded", e => {
+  $$("code.language-math").forEach(code => {
+    katex.render(code.innerText, code)
+  })
+})
